eb29ff5596ca8b3adcbf0dbf0a693fee9109ac61
leonardespi <leonardespicass@gmail.com>
2026-05-21 23:58:06 -0600

    config: exclude webstore from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
+      filter: (page) => !page.includes('/webstore'),
     }),
     mdx(),
   ],
